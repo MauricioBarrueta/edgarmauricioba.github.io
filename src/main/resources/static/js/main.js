74d9d373fd6f6c7e27219d48594fc736c37374d0
Mauricio <mauba22@outlook.com>
2022-07-18 11:42:26 -0500

    Commit GitHub Pages npm-gh-pages

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,24 +1,9 @@
-/* Botón Scroll To Top */
-const btnSTT = document.querySelector('.ScrollToTop');
-btnSTT.addEventListener('click', function() {
-    window.scrollTo({
-        top: 0, left: 0,
-        behavior: "smooth"
-    });
-});
-/* Hace visible el bóton al iniciar el scroll */
-window.addEventListener('scroll', e => {
-    btnSTT.style.display = window.scrollY > 150 ? 'block' : 'none';    
-}); 
-
-/* Para dar el efecto 'sticky' a la navbar al pasar por el elemento oculto span con clase .trigger */
-(new IntersectionObserver(function(e,o){
-    if (e[0].intersectionRatio > 0){
-        document.documentElement.removeAttribute('class');
-    } else {
-        document.documentElement.setAttribute('class','stuck');
-    };
-})).observe(document.querySelector('.trigger'));
+/* Oculta la animación al cargar toda la página y además le agrega 5s */
+$(window).on('load', function () {
+    setTimeout(function() {        
+        $('#loading').hide("fast");                             
+    }, 5000);    
+})
 
 /* Resalta en la navbar la sección en la que se encuentra */
 $(document).ready(function () {
@@ -26,8 +11,37 @@ $(document).ready(function () {
         $('.navbar a.active').removeClass('active');
         $(this).addClass('active');
     });
-    $('body').scrollspy({ target: '.navbar' })
+    $('body').scrollspy({ target: '.navbar' });
+
+    /* Efecto parallax del footer */
+    siteFooter();	
+	$(window).resize(function() { 
+        siteFooter(); 
+    });	
+	function siteFooter() {
+		var siteContent = $('.main');
+		var siteFooter = $('#footer');
+		var siteFooterHeight = siteFooter.height();
+		siteContent.css({ "margin-bottom" : siteFooterHeight + 50 });
+	};
 })
+
+/* Botón Scroll To Top */
+const btnSTT = document.querySelector('.ScrollToTop');
+btnSTT.addEventListener('click', function() {
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+});
+/* Hace visible el bóton al iniciar el scroll */
+window.addEventListener('scroll', e => {
+    btnSTT.style.display = window.scrollY > 150 ? 'block' : 'none'; 
+}); 
+
+/* Para dar el efecto 'sticky' a la navbar al pasar por el elemento oculto span con clase .trigger */
+(new IntersectionObserver(function(e,o) {
+    if (e[0].intersectionRatio > 0) { document.documentElement.removeAttribute('class');
+    } else { document.documentElement.setAttribute('class','stuck');
+    };
+})).observe(document.querySelector('.trigger'));
 
 /* Obtiene el atributo src de la imagen seleccionada para pasarla al modal y mostrarlo */
 document.addEventListener("click", function(e) {
@@ -45,22 +59,36 @@ document.addEventListener("click", function(e) {
 $(".img-item").hover(function() {
     $(this).closest(".img-item").css("z-index", 1);    
     $(this).animate({ height: "210", width: "310"}, "fast");
-}, 
-function() {
+}, function() {
     $(this).closest(".img-item").css("z-index", 0);
     $(this).animate({ height: "200", width: "300" }, "fast");
 });
 
+/* Función del botón para mostrar/ocultar los proyectos */
+function showMoreProjects() {
+    var cardsVisible = document.getElementById("visible");
+    var cardsHidden = document.getElementById("hidden");
+    var btnShow = document.getElementById("show-more-btn");
+
+    if (cardsVisible.style.display === "none") {
+        btnShow.innerHTML = "<i class='fa-solid fa-caret-down fa-bounce'></i>"; btnShow.setAttribute("title", "Expandir - Expand");  
+        cardsVisible.style.display = "flex"; cardsHidden.style.display = "none";
+    } else {
+        cardsVisible.style.display = "none";
+        btnShow.innerHTML = "<i class='fa-solid fa-caret-up fa-bounce'></i>"; btnShow.setAttribute("title", "Reducir - Reduce");        
+        cardsHidden.style.display = "flex"; cardsHidden.style.flexWrap = "wrap";
+        cardsHidden.style.alignContent = "center"; cardsHidden.style.justifyContent = "space-around"; cardsHidden.style.gap = "15px";    
+    }
+}
+
 /* Función que obtiene los datos del formulario para enviar el correo electrónico */
 const $form = document.querySelector('#c-form');
 const btnEmailTo = document.querySelector('#myEmail');
-
 $form.addEventListener('submit', onSubmit)
 
 function onSubmit(event) {
     event.preventDefault()
     const form = new FormData(this)
-
     const btnSendEmail = $('#btnSendEmail')
     const btnHtmlElements = btnSendEmail.html() // Almacena las caracteristicas del botón    
     $(btnSendEmail).html('<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>&nbsp; Procesando...').prop('disabled', true)
@@ -70,27 +98,19 @@ function onSubmit(event) {
         $(btnSendEmail).html(btnHtmlElements).prop('disabled', false) // Regresa el btn a como estaba originalmente
         btnEmailTo.setAttribute('href', `mailto:mauba22@outlook.com?subject=Quiero contactar: ${form.get('name')} - ${form.get('email')}&body=${form.get('message')}`)
         btnEmailTo.click();
-    }, 
-    4000) // 4 segundos
+    }, 4000) 
 }
 
-/* Footer con efecto 'Parallax' */
-$(document).ready(function() {
-  siteFooter();	
-	$(window).resize(function() {
-		siteFooter();
-	});	
-	function siteFooter() {
-		var siteContent = $('.main');
-		var siteFooter = $('#footer');
-		var siteFooterHeight = siteFooter.height();
+/* Función para copiar el email al portapapeles */
+function copyEmail() {
+    var copyText = document.getElementById("email-copy");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /* Para móviles */   
+    navigator.clipboard.writeText(copyText.value); 
+    alert(copyText.value + " copiado al portapapeles");
+}
 
-		siteContent.css({
-			"margin-bottom" : siteFooterHeight + 50
-		});
-	};
-});
-
+/* Muestra un modal con un 'loader' por 3s al traducir la página */
 function modalSpinner() {
     $('.modalSpinner').modal('show');
     setTimeout(function () {        
@@ -98,13 +118,14 @@ function modalSpinner() {
     }, 3000);
  }
 
+ /* Función para traducir entre español e inglés */
 function switchLang(lang) {
-    $("[data-" + lang + "]").text(function(i, e) {
-        return $(this).data(lang);
+    $("[data-" + lang + "]").text(function(i, e) { 
+        return $(this).data(lang); 
     });
 }
 switchLang("es");
-
+/* Ejecuta la función para traducir y muestra el modal de carga al mismo tiempo */
 $(".switchlang").click(function() {
     modalSpinner();  
     switchLang($(this).data("lang"))
